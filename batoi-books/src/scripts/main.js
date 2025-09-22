2a843d27c580ad6775c1c231e07c4fa2cee7c486
Martí Moll <marticlasse@gmail.com>
2025-09-22 20:27:55 +0200

    [MOD] modificats la gran majoria de fitxers, eliminats altres

---
+++ batoi-books/src/scripts/main.js
@@ -0,0 +1,13 @@
+import '../styles/style.css'
+import batoiLogo from '../../public/logoBatoi.png'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+      <img src="${batoiLogo}" class="logo" alt="Batoi logo" />
+    <h1>BatoiBooks</h1>
+    <p class="texto">
+      Abre la consola para ver el resultado
+    </p>
+  </div>
+`
