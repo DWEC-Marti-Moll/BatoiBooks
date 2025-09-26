8cfbf7f3752e74237337e5d2d52728dad45f9f2c
Martí Moll <marticlasse@gmail.com>
2025-09-26 19:52:47 +0200

    [MOD] acabat fitxer "functions.js"

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,49 +1,53 @@
 'use strict';
-import data from '../services/datos.js';
 
-function getBookById(books, number){
+const getBookById = (books, number) =>{
     const book = books.find(id => id.id === number);
     if(!book) throw new Error('Book not found');
     return book;
 }
-function getBookIndexById(books, number){
+const getBookIndexById = (books, number) =>{
     const index = books.findIndex(id => id.id === number);
     if(index === -1) throw new Error('Book not found');
     return index;
 }
-function bookExists(books, userId, moduleCode){
-    return books.some(book => book.userId === userId && book.moduleCode === moduleCode);
-}
-function booksFromUser(books, userId){
-    return books.filter(book => book.userId === userId);
-}
-function booksFromModule(books, moduleCode){
-    return books.filter(book => book.moduleCode === moduleCode);
-}
-function booksCheeperThan(books, price){
-    return books.filter(book => book.price < price);
-}
-function booksWithStatus(books, status){
-    return books.filter(book => book.status === status);
-}
-function averagePriceOfBooks(books){
+const bookExists = (books, userId, moduleCode) => books.some(book => book.userId === userId && book.moduleCode === moduleCode);
+const booksFromUser = (books, userId) => books.filter(book => book.userId === userId);
+const booksFromModule = (books, moduleCode) => books.filter(book => book.moduleCode === moduleCode);
+const booksCheeperThan = (books, price) => books.filter(book => book.price < price);
+const booksWithStatus = (books, status) => books.filter(book => book.status === status);
+const averagePriceOfBooks = (books) =>{
     if (books.length == 0) return '0.00 €';
     const total = books.reduce((sum, book) => sum + book.price, 0);
     const average = total / books.length;
     return average.toFixed(2) + ' €';
 }
-function booksOfTypeNotes(books){
-    return books.filter(book => book.publisher === 'Apunts');
+const booksOfTypeNotes = (books) => books.filter(book => book.publisher === 'Apunts');
+const booksNotSold = (books) => books.filter(book => book.soldDate === '');
+const incrementPriceOfbooks = (books, percentage) =>{
+    return books.map(book => ({
+        ...book, 
+        price: book.price + book.price * percentage}));
 }
-function booksNotSold(books){
-    return books.filter(book => book.soldDate === '');
+const getUserById = (users, userId) => {
+    const user = users.find(id => id.id === userId);
+    if(!user) throw new Error('User not found');
+    return user;
 }
-function incrementPriceOfbooks(books, percentage){
-    return books.map(book => book.price * percentage/100);
+const getUserIndexById = (users, userId) =>{
+    const user = users.findIndex(id => id.id === userId);
+    if(user == -1) throw new Error('User not found');
+    return user;
 }
-function getUserById(users, userId){}
-function getUserByNickName(users, nick){}
-function getModuleByCode(modules, moduleCode){}
+const getUserByNickName = (users, nick) =>{
+    const user = users.find(name => name.nick === nick);
+    if(!user) throw new Error('User not found');
+    return user;
+}
+const getModuleByCode = (modules, moduleCode) =>{
+    const module = modules.find(mod => mod.code === moduleCode);
+    if(!module) throw new Error('Module not found');
+    return module;
+}
 
 export{
     getBookById,
@@ -58,6 +62,7 @@ export{
     booksNotSold,
     incrementPriceOfbooks,
     getUserById,
+    getUserIndexById,
     getUserByNickName,
     getModuleByCode
 }
