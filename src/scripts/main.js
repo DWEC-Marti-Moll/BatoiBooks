6feab376263580a6ac5a2409d878ab928e0049b6
Martí Moll <marticlasse@gmail.com>
2025-09-29 18:57:16 +0200

    [END] parte arrays terminada

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
-import '../styles/style.css'
-import batoiLogo from '/logoBatoi.png'
-
+import '../styles/style.css';
+import batoiLogo from '/logoBatoi.png';
+import * as functions from './functions.js';
+import data from '../services/datos.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,3 +12,9 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
+console.log("Todos los libros del usuario 4:");
+console.log(functions.booksFromUser(data.books, 4));
+console.log("Todos los libros del módulo 5021 que están en buen estado:");
+console.log(functions.booksWithStatus(functions.booksFromModule(data.books, "5021"), "good"));
+console.log("Incrementar un 10% el precio de los libros");
+console.log(functions.incrementPriceOfbooks(data.books, 0.1));
